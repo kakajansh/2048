ff426e104004fab7192593e1e8ae2f2589ee0d3a
Sayawan Devs <sayawan.devs@gmail.com>
2014-12-28 20:10:43 +0200

    Initial commit

---
+++ 2048 hoca/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace _2048_hoca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameTypeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x6ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x7ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameTypeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(605, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameTypeToolStripMenuItem
+            // 
+            this.gameTypeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x3ToolStripMenuItem,
+            this.x4ToolStripMenuItem,
+            this.x5ToolStripMenuItem,
+            this.x6ToolStripMenuItem,
+            this.x7ToolStripMenuItem,
+            this.x8ToolStripMenuItem});
+            this.gameTypeToolStripMenuItem.Name = "gameTypeToolStripMenuItem";
+            this.gameTypeToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.gameTypeToolStripMenuItem.Text = "Start";
+            this.gameTypeToolStripMenuItem.Click += new System.EventHandler(this.gameTypeToolStripMenuItem_Click);
+            // 
+            // x3ToolStripMenuItem
+            // 
+            this.x3ToolStripMenuItem.Name = "x3ToolStripMenuItem";
+            this.x3ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.x3ToolStripMenuItem.Text = "3x3";
+            // 
+            // x4ToolStripMenuItem
+            // 
+            this.x4ToolStripMenuItem.Name = "x4ToolStripMenuItem";
+            this.x4ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.x4ToolStripMenuItem.Text = "4x4";
+            // 
+            // x5ToolStripMenuItem
+            // 
+            this.x5ToolStripMenuItem.Name = "x5ToolStripMenuItem";
+            this.x5ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.x5ToolStripMenuItem.Text = "5x5";
+            // 
+            // x6ToolStripMenuItem
+            // 
+            this.x6ToolStripMenuItem.Name = "x6ToolStripMenuItem";
+            this.x6ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.x6ToolStripMenuItem.Text = "6x6";
+            // 
+            // x7ToolStripMenuItem
+            // 
+            this.x7ToolStripMenuItem.Name = "x7ToolStripMenuItem";
+            this.x7ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.x7ToolStripMenuItem.Text = "7x7";
+            // 
+            // x8ToolStripMenuItem
+            // 
+            this.x8ToolStripMenuItem.Name = "x8ToolStripMenuItem";
+            this.x8ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.x8ToolStripMenuItem.Text = "8x8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(605, 519);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameTypeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x6ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x7ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x8ToolStripMenuItem;
+    }
+}
+
---
+++ 2048 hoca/Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048_hoca
+{
+    public partial class Form1 : Form
+    {
+
+        private volatile bool toBreak = false;
+        private volatile bool toContinue = false;
+        private volatile int boardSize = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ekrana_kutu_koy(4);
+
+            foreach (ToolStripMenuItem item in menuStrip1.Items)
+            {
+                Subscribe(item, gameTypeToolStripMenuItem_Click);
+            }
+        }
+
+        private static void Subscribe(ToolStripMenuItem item, EventHandler eventHandler)
+        {
+            // If leaf, add click handler
+            if (item.DropDownItems.Count == 0)
+                item.Click += eventHandler;
+            // Otherwise recursively subscribe
+            else foreach (ToolStripMenuItem subItem in item.DropDownItems)
+                    Subscribe(subItem, eventHandler);
+        }
+
+        // See more http://stackoverflow.com/a/5696497
+        private void gameTypeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string tiklama = (sender as ToolStripMenuItem).Text;
+            Console.WriteLine(tiklama);
+            if (tiklama != "Start")
+            {
+                boardSize = int.Parse(tiklama.Substring(0, 1));
+                ekrana_kutu_koy(boardSize);
+            }
+        }
+
+        void ekrana_kutu_koy(int n)
+        {
+            int kutusay = 1;
+
+            for (int i = this.Controls.Count - 1; i >= 0; i--)
+                if (this.Controls[i] is TextBox)
+                    this.Controls.Remove(this.Controls[i]);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    TextBox tb = new TextBox();
+                    tb.Name = "txt_kutu" + kutusay;
+                    tb.Multiline = true;
+                    tb.Height = 50;
+                    tb.Width = 50;
+                    //tb.MaxLength = 1;
+                    //tb.Text = kutusay.ToString();
+                    tb.TextAlign = HorizontalAlignment.Center;
+                    tb.Font = new System.Drawing.Font(tb.Font, FontStyle.Bold);
+                    tb.Font = new System.Drawing.Font(tb.Font.FontFamily, 16);
+                    Point yer;
+                    yer = new System.Drawing.Point(j * tb.Height + 30, i * tb.Width + 30);
+                    tb.Location = yer;
+                    this.Controls.Add(tb);
+                    kutusay++;
+                }
+            }
+            this.Width = 50 * n + 75;
+            this.Height = 50 * n + 90;
+
+            rand_sayi();
+        }
+
+        void oyuna_basla(int n)
+        {
+            //Random random = new Random();
+            //while(true) {
+            //    int sayi1 = random.Next(1, n*n + 1);
+            //    int sayi2 = random.Next(1, n*n + 1);
+
+            //    if (sayi1 != sayi2) {
+            //        this.Controls["txt_kutu"+sayi1].Text = "2";
+            //        this.Controls["txt_kutu"+sayi2].Text = "2";
+            //        break;
+            //    }
+            //}
+            //this.Controls["txt_kutu6"].Text = "2";
+            //this.Controls["txt_kutu11"].Text = "2";
+        }
+
+        void rand_sayi()
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int sayi1 = random.Next(1, boardSize*boardSize+1);
+                int sayi2 = random.Next(1, boardSize*boardSize+1);
+
+                if (sayi1 != sayi2)
+                {
+                    TextBox birinci = this.Controls["txt_kutu" + sayi1] as TextBox;
+                    TextBox ikinci = this.Controls["txt_kutu" + sayi2] as TextBox;
+                    if (birinci.Text == "") { birinci.Text = "2"; }
+                    if (ikinci.Text == "") { ikinci.Text = "2"; }
+                    break;
+                }
+            }
+        }
+
+        void renklendir()
+        {
+            for (int i = 1; i <= boardSize*boardSize; i++)
+            {
+                TextBox kutu = this.Controls["txt_kutu" + i] as TextBox;
+                int sayi = 0;
+
+                if (kutu.Text != "")
+                {
+                    sayi = int.Parse(kutu.Text);
+                }
+                else
+                {
+                    kutu.BackColor = Color.White;
+                    kutu.ForeColor = Color.Black;
+                    continue;
+                }
+
+                switch (sayi)
+                {
+                    case 2:
+                        {
+                            kutu.BackColor = Color.GhostWhite;
+                            kutu.ForeColor = Color.Black;
+                            break;
+                        }
+                    case 4:
+                        {
+                            kutu.BackColor = Color.PapayaWhip;
+                            kutu.ForeColor = Color.Black;
+                            break;
+                        }
+                    case 8:
+                        {
+                            kutu.BackColor = Color.LightSalmon;
+                            kutu.ForeColor = Color.White;
+                            break;
+                        }
+                    case 16:
+                        {
+                            kutu.BackColor = Color.Coral;
+                            kutu.ForeColor = Color.White;
+                            break;
+                        }
+                    case 32:
+                        {
+                            kutu.BackColor = Color.Tomato;
+                            kutu.ForeColor = Color.White;
+                            break;
+                        }
+                    case 64:
+                        {
+                            kutu.BackColor = Color.Red;
+                            kutu.ForeColor = Color.White;
+                            break;
+                        }
+                    case 128:
+                        {
+                            kutu.BackColor = Color.Gold;
+                            kutu.ForeColor = Color.Black;
+                            break;
+                        }
+                    case 256:
+                        {
+                            kutu.BackColor = Color.Goldenrod;
+                            kutu.ForeColor = Color.Black;
+                            break;
+                        }
+                    default:
+                        {
+                            kutu.BackColor = Color.White;
+                            kutu.ForeColor = Color.Black;
+                            break;
+                        }
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                Console.WriteLine("****** UP KEY ******");
+                yukari_tus();
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                Console.WriteLine("****** DOWN KEY ******");
+                asagi_tus();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                Console.WriteLine("****** RIGHT KEY ******");
+                sag_tus();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                Console.WriteLine("****** LEFT KEY ******");
+                sol_tus();
+            }
+
+            rand_sayi();
+            renklendir();
+        }
+
+        void asagi_tus()
+        {
+            int basla = boardSize * boardSize - boardSize + 1;
+
+            for (int i = 1; i <= boardSize; i++)
+            {
+                for (int j = basla; j >= i; j = j - boardSize)
+                {
+                    toBreak = false;
+                    toContinue = false;
+
+                    for (int k = j - boardSize; k >= i; k = k - boardSize)
+                    {
+                        if (j <= k) { continue; }
+
+                        string islem = "DOWN";
+                        tekrarlanan_kisim(islem, j, k);
+
+                        if (toBreak) break;
+
+                        if (toContinue) continue;
+                    }
+                }
+                basla = basla + 1;
+            }
+        }
+
+        void yukari_tus()
+        {
+            int basla = 1;
+
+            for (int i = 1; i <= boardSize; i++)
+            {
+                for (int j = basla; j <= basla + (boardSize * (boardSize - 1)); j = j + boardSize)
+                {
+                    toBreak = false;
+                    toContinue = false;
+
+                    for (int k = j + boardSize; k <= basla + (boardSize * boardSize - 1); k = k + boardSize)
+                    {
+                        if (j >= k) { continue; }
+
+                        string islem = "UP";
+                        tekrarlanan_kisim(islem, j, k);
+
+                        if (toBreak) break;
+
+                        if (toContinue) continue;
+
+                    }
+                }
+                basla = basla + 1;
+            }
+        }
+
+        void sol_tus()
+        {
+            int basla = 1;
+
+            for (int i = 1; i <= boardSize; i++)
+            {
+                for (int j = basla; j <= basla + (boardSize - 1); j++)
+                {
+                    toBreak = false;
+                    toContinue = false;
+
+                    for (int k = j + 1; k <= basla + (boardSize - 1); k++)
+                    {
+                        if (j >= k) { continue; }
+
+                        string islem = "LEFT";
+                        tekrarlanan_kisim(islem, j, k);
+
+                        if (toBreak) break;
+
+                        if (toContinue) continue;
+
+                    }
+                }
+                basla = basla + 4;
+            }
+        }
+
+        void sag_tus()
+        {
+            int basla = 1;
+
+            for (int i = 1; i <= boardSize; i++)
+            {
+                for (int j = basla + (boardSize - 1); j >= basla; j--)
+                {
+                    toBreak = false;
+                    toContinue = false;
+
+                    for (int k = j - 1; k >= basla; k--)
+                    {
+                        string islem = "RIGHT";
+                        tekrarlanan_kisim(islem, j, k);
+
+                        if (toBreak) break;
+
+                        if (toContinue) continue;
+
+                    }
+                }
+                basla = basla + boardSize;
+            }
+        }
+
+        // XXX clean up here
+        // [islem] just for loggin
+        // [toContinue, toBreak] for breaking loops above
+        // See more http://stackoverflow.com/a/14226843
+        void tekrarlanan_kisim(string islem, int j, int k)
+        {
+            Console.WriteLine("[" + islem + "] - " + j + " " + k);
+            TextBox birinci = this.Controls["txt_kutu" + j] as TextBox;
+            TextBox ikinci = this.Controls["txt_kutu" + k] as TextBox;
+
+            if (birinci.Text == "" && ikinci.Text != "")
+            {
+                //Console.WriteLine("[BOS] - " + j + " " + k);
+                birinci.Text = ikinci.Text;
+                ikinci.Text = "";
+                //toContinue = true;
+            }
+
+            if (birinci.Text != "" && ikinci.Text != "" && birinci.Text != ikinci.Text)
+            {
+                //Console.WriteLine("[BREAK] - " + j + " " + k);
+                toBreak = true;
+            }
+
+            if (birinci.Text != "" && ikinci.Text != "" && birinci.Text == ikinci.Text)
+            {
+                //Console.WriteLine("[TOPLA] - " + j + " " + k);
+                int sum = Int32.Parse(birinci.Text) + Int32.Parse(ikinci.Text);
+                birinci.Text = sum.ToString();
+                ikinci.Text = "";
+            }
+        }
+
+    }
+}
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = basla; j < basla+3; j++)
+//    {
+//        if (j + 1 > basla + 4) break;
+//        TextBox birinci = this.Controls["txt_kutu" + j] as TextBox;
+//        TextBox ikinci = this.Controls["txt_kutu" + (j + 1)] as TextBox;
+
+//        if (birinci.Text != "" && ikinci.Text == "")
+//        {
+//            ikinci.Text = birinci.Text;
+//            birinci.Text = "";
+//            Console.WriteLine("bosluk" + j);
+//            //break;
+//        }
+//        else if (birinci.Text != "" && ikinci.Text != "" && birinci.Text == ikinci.Text)
+//        {
+//            int sum = int.Parse(birinci.Text) + int.Parse(ikinci.Text);
+//            ikinci.Text = sum.ToString();
+//            birinci.Text = "";
+//            Console.WriteLine("arti" + j);
+//            break;
+//        }
+//        else
+//        {
+//            Console.WriteLine("else" + j);
+//        }
+
+//    }
+//    basla = basla + 4;
+//}
